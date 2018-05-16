50376ae727210722d29579dfc9763ad28758f42a
michael hohl <mhohl@rocketmail.com>
2018-05-16 16:49:41 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,18 @@
+# Your TextAnalyzer model code will go here.
+class PigLatinizer
+  attr_reader :text
+
+  # def initialize(phrase)
+  #   @text = phrase.downcase
+  # end
+
+  def piglagtinize_word(word)
+    match = word.match(/^((?:qu|[bcdfghjklmnpqrstvwxz])*)(.*)$/)
+    match ? match[2] + match[1] + "ay" : word
+  end
+
+  def piglatinize(phrase)
+    words = phrase.split(" ")
+    words.map { |word| piglagtinize_word(word) }.join(" ")
+  end
+end
